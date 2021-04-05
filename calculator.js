4fb1451a8e891278719832a0e90537b90dfbf68b
pwj-v <melissa.velasco093@gmail.com>
2021-04-05 14:35:55 -0400

    Operate function takes an operator and two numbers and calls one of the math functions on the numbers.

--- calculator.js
+++ calculator.js
@@ -20,3 +20,29 @@ function divide (x) {
     });
     return math;
 }
+
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case add:
+            console.log('addition works');
+            return add(num1, num2);
+            break;
+        case subtract:
+            console.log('subtraction works');
+            return subtract(num1, num2);
+            break;
+        case multiply:
+            console.log('multiplication works');
+            let multiplyArray = [num1, num2];
+            console.log(multiplyArray);
+            return multiply(multiplyArray);
+            break;
+        case divide:
+            console.log('division works');
+            let divideArray = [num1, num2];
+            return divide(divideArray);
+            break;
+        default:
+            console.log('something went wrong');
+    }
+}
