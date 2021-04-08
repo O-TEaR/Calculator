a74cff57eaf863ca507f599e89ab1c41c2d644a5
pwj-v <melissa.velasco093@gmail.com>
2021-04-07 20:16:01 -0400

    Calculator is able to calculate expressions. Clear button wipes any existing data.

--- calculator.js
+++ calculator.js
@@ -21,23 +21,23 @@ function divide (x) {
     return math;
 }
 
-function operate (operator, num1, num2) {
+function operate (num1, operator, num2) {
     switch (operator) {
-        case add:
+        case '+':
             console.log('addition works');
             return add(num1, num2);
             break;
-        case subtract:
+        case '-':
             console.log('subtraction works');
             return subtract(num1, num2);
             break;
-        case multiply:
+        case '*':
             console.log('multiplication works');
             let multiplyArray = [num1, num2];
             console.log(multiplyArray);
             return multiply(multiplyArray);
             break;
-        case divide:
+        case '/':
             console.log('division works');
             let divideArray = [num1, num2];
             return divide(divideArray);
@@ -53,7 +53,97 @@ let buttons = document.querySelectorAll('button');
 let buttonValue;
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        console.log(button.textContent);
         buttonValue = button.textContent;
-        display.textContent = `${buttonValue}`;
+        check();
+        display.textContent = `${expressionArray.join('')}`;
     });
 });
+
+const validateNumber = /[0-9]/;
+const validateOperator = /[\*\+\-\/]/;
+const validateEqual = /[=]/;
+const validateClear = /Clear/
+let expressionArray = [];
+let operateVals = [];
+let aNumber = [];
+let bNumber = [];
+let answer;
+
+function check() {
+    if (validateNumber.test(buttonValue)) {
+        console.log('it is a number');
+        expressionArray.push(Number(buttonValue));
+        placeVals();
+    }
+    else if (validateOperator.test(buttonValue)) {
+        console.log('it is an operator')
+        expressionArray.push(buttonValue);
+        checkOperator();
+    }
+    else if (validateEqual.test(buttonValue)) {
+        console.log('equal button');
+        checkExpression ();
+        console.log(answer);
+        display.textContent = `${answer}`;
+        expressionArray = [];
+        operateVals = [];
+        aNumber = [];
+        bNumber = [];
+    }
+    else if (validateClear.test(buttonValue)) {
+        console.log('you hit clear');
+        expressionArray = [];
+        operateVals = [];
+        aNumber = [];
+        bNumber = [];
+    }
+    console.log(expressionArray);
+    console.log(`operate values is ${operateVals}`);
+    console.log(`Anumber is ${aNumber} and bNumber is ${bNumber}`);
+}
+
+function checkOperator() {
+    if (operateVals.length == 2 && bNumber.length > 0){
+        bNumber = bNumber.join('');    
+        operateVals[2] = Number(bNumber);    
+        console.log(operateVals.length);      
+        answer = operate(operateVals[0], operateVals[1], operateVals[2]);
+        console.log(`ANSWER IS ${answer}`);
+        replaceFirstNum();
+    }    
+    else if (operateVals.length == 0 && aNumber.length > 0) {
+        aNumber = aNumber.join('');    
+        operateVals.push(Number(aNumber));        
+        operateVals.push(buttonValue);            
+    } else {    
+        operateVals[1] = buttonValue;    
+      }      
+    }            
+
+function placeVals () {
+    if (operateVals.length == 0) {
+        aNumber.push(buttonValue);
+    }
+    else if (operateVals.length == 2) {
+        bNumber.push(buttonValue);
+        bNumber.join('');
+        console.log(bNumber);
+    }
+}
+
+function replaceFirstNum() {
+    operateVals = [answer, buttonValue];
+    aNumber = answer;
+    bNumber = [];
+}
+
+function checkExpression () {
+    if (operateVals.length == 2) {
+        bNumber = bNumber.join('');    
+        operateVals[2] = Number(bNumber);
+        answer = operate(operateVals[0], operateVals[1], operateVals[2]);
+        console.log(`ANSWER IS ${answer}`);
+        replaceFirstNum();
+    }
+}
